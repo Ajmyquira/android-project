430f6b829926d8e62aebd8b8daeb4f1dab42dbbb
Antonyjuan Miranda Yquira <antonyjuan.miranda@ucsp.edu.pe>
2022-05-28 20:03:32 -0500

    Register pet to Firebase added

--- app/src/main/java/com/example/android_project/CreatePetActivity.java
+++ app/src/main/java/com/example/android_project/CreatePetActivity.java
@@ -1,14 +1,82 @@
 package com.example.android_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreatePetActivity extends AppCompatActivity {
+
+    Button btn_add;
+    EditText name, age, color;
+    private FirebaseFirestore mfirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_pet);
+
+        this.setTitle("Create Pet");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mfirestore = FirebaseFirestore.getInstance();
+
+        name = findViewById(R.id.name);
+        age = findViewById(R.id.age);
+        color = findViewById(R.id.color);
+        btn_add = findViewById(R.id.btn_add);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String namepet = name.getText().toString().trim();
+                String agepet = age.getText().toString().trim();
+                String colorpet = color.getText().toString().trim();
+
+                if(namepet.isEmpty() && agepet.isEmpty() && colorpet.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Insert data", Toast.LENGTH_LONG).show();
+                }else{
+                    postPet(namepet, agepet, colorpet);
+                }
+            }
+        });
+
+    }
+
+    private void postPet(String namepet, String agepet, String colorpet) {
+        Map<String,Object> map = new HashMap<>();
+        map.put("name", namepet);
+        map.put("age", agepet);
+        map.put("color", colorpet);
+
+        mfirestore.collection("pet").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"Added successfully",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return false;
     }
 }
